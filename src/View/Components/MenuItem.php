271629a8ba71b33ff5e21d6c2a99c0c9de97519e
Jacob Martella <me@jacobmartella.com>
2025-07-29 17:49:23 -0500

    Added in features for menu items

--- src/View/Components/MenuItem.php
+++ src/View/Components/MenuItem.php
@@ -21,6 +21,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Str;
 use Illuminate\View\Component;
+use ArtisanPackUI\Accessibility\A11y;
 /**
  * MenuItem Class
  *
@@ -31,64 +32,93 @@ use Illuminate\View\Component;
 
 class MenuItem extends Component
 {
-    public string $uuid;
+	public string $uuid;
+	// For theme colors like 'primary'
+	public array $themeColorClasses = [];
+	// For dynamic hex colors from a database
+	public ?string $dynamicBgColor = null;
+	public ?string $dynamicTextColor = null;
 
-    public function __construct(
-        public ?string $id = null,
-        public ?string $title = null,
-        public ?string $icon = null,
-        public ?string $iconClasses = null,
-        public ?string $spinner = null,
-        public ?string $link = null,
-        public ?string $route = null,
-        public ?bool $external = false,
-        public ?bool $noWireNavigate = false,
-        public ?string $badge = null,
-        public ?string $badgeClasses = null,
-        public ?bool $active = false,
-        public ?bool $separator = false,
-        public ?bool $hidden = false,
-        public ?bool $disabled = false,
-        public ?bool $exact = false
-    ) {
-        $this->uuid = "artisanpack" . md5(serialize($this)) . $id;
-    }
+	public function __construct(
+		public ?string $id = null,
+		public ?string $title = null,
+		public ?string $icon = null,
+		public ?string $iconClasses = null,
+		public ?string $spinner = null,
+		public ?string $link = null,
+		public ?string $route = null,
+		public ?bool $external = false,
+		public ?bool $noWireNavigate = false,
+		public ?string $badge = null,
+		public ?string $badgeClasses = null,
+		public ?bool $active = false,
+		public ?bool $separator = false,
+		public ?bool $hidden = false,
+		public ?bool $disabled = false,
+		public ?bool $exact = false,
+		public ?string $bgColor = null
+	) {
+		$this->uuid = "artisanpack" . md5(serialize($this)) . $id;
 
-    public function spinnerTarget(): ?string
-    {
-        if ($this->spinner == 1) {
-            return $this->attributes->whereStartsWith('wire:click')->first();
-        }
+		if ($this->bgColor) {
+			// Check if it's a dynamic hex color
+			if (str_starts_with($this->bgColor, '#')) {
+				$this->dynamicBgColor = $this->bgColor;
+				$this->dynamicTextColor = generateAccessibleTextColor($this->bgColor);
+			} else {
+				// Otherwise, treat as a theme color and generate Tailwind classes
+				$baseBgClass = "bg-{$this->bgColor}";
+				$baseTextColorClass = "text-{$this->bgColor}-content";
 
-        return $this->spinner;
-    }
+				$this->themeColorClasses = [
+					"active-bg"    => $baseBgClass,
+					"active-text"  => $baseTextColorClass,
+					"hover-focus" => [
+						"hover:{$baseBgClass}",
+						"hover:{$baseTextColorClass}",
+						"focus:{$baseBgClass}",
+						"focus:{$baseTextColorClass}",
+					]
+				];
+			}
+		}
+	}
 
-    public function routeMatches(): bool
-    {
-        if ($this->link == null) {
-            return false;
-        }
+	public function spinnerTarget(): ?string
+	{
+		if ($this->spinner == 1) {
+			return $this->attributes->whereStartsWith('wire:click')->first();
+		}
 
-        if ($this->route) {
-            return request()->routeIs($this->route);
-        }
+		return $this->spinner;
+	}
 
-        $link = url($this->link ?? '');
-        $route = url(request()->url());
+	public function routeMatches(): bool
+	{
+		if ($this->link == null) {
+			return false;
+		}
 
-        if ($link == $route) {
-            return true;
-        }
+		if ($this->route) {
+			return request()->routeIs($this->route);
+		}
 
-        return ! $this->exact && $this->link != '/' && Str::startsWith($route, $link);
-    }
+		$link = url($this->link ?? '');
+		$route = url(request()->url());
 
-    public function render(): View|Closure|string
-    {
-        if ($this->hidden === true) {
-            return '';
-        }
+		if ($link == $route) {
+			return true;
+		}
 
-        return view('livewire-ui-components::components.menu-item');
-    }
+		return ! $this->exact && $this->link != '/' && Str::startsWith($route, $link);
+	}
+
+	public function render(): View|Closure|string
+	{
+		if ($this->hidden === true) {
+			return '';
+		}
+
+		return view('livewire-ui-components::components.menu-item');
+	}
 }
